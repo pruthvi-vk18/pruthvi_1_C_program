0f5f4bc11c755879180041a9c9f059da52d8446c
pruthvi-vk18 <pruthvi000123@gmail.com>
2024-11-26 00:42:27 -0800

    Add files via upload

---
+++ age.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int age;
+    printf("Enter your age: ");
+    scanf("%d", &age);
+if (age >18) {
+    if(age<30){
+        printf("you are a young adult");
+    }
+}
+    return 0;
+}
