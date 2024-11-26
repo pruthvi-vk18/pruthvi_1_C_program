30136c821353d6f70aea6eb4174fe1af59fa237e
pruthvi-vk18 <pruthvi000123@gmail.com>
2024-11-26 02:21:23 -0800

    Add files via upload

---
+++ string.c
@@ -0,0 +1,30 @@
+include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    int len, isPalindrome = 1;
+
+    
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    len = strlen(str);
+
+    
+    for (int i = 0; i < len / 2; i++) {
+        if (str[i] != str[len - i - 1]) {
+            isPalindrome = 0; 
+            break;
+        }
+    }
+
+    
+    if (isPalindrome) {
+        printf("The string \"%s\" is a palindrome.\n", str);
+    } else {
+        printf("The string \"%s\" is not a palindrome.\n", str);
+    }
+
+    return 0;
+}
